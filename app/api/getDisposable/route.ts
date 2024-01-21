4d63bb27ca57d8a464676b076160ae5d04ddb604
Ivan Roznov <van.roznov@gmail.com>
2024-01-21 17:49:09 +0300

    configured api

---
+++ app/api/getDisposable/route.ts
@@ -0,0 +1,26 @@
+import { verifyLaunchParams } from "@/app/lib/auth";
+import prisma from "../db";
+
+export async function POST(request: Request): Promise<Response> {
+  try {
+    const headers = request.headers;
+    const verified = verifyLaunchParams(
+      Object.fromEntries(headers),
+      process.env.SECRET_KEY ?? "defaultkey"
+    );
+    if (!verified) {
+      throw "Not verified";
+    }
+    return Response.json({
+      status: "success",
+      user: await prisma.disposable.findFirst({
+        where: { userId: Number(headers.get("vk_user_id")) },
+      }),
+    });
+  } catch (e) {
+    console.error(e);
+    return Response.json({
+      status: "error",
+    });
+  }
+}
---
+++ app/api/getUser/route.ts
@@ -0,0 +1,27 @@
+import { verifyLaunchParams } from "@/app/lib/auth";
+import { PrismaClient } from "@prisma/client";
+import prisma from "../db";
+
+export async function POST(request: Request): Promise<Response> {
+  try {
+    const headers = request.headers;
+    const verified = verifyLaunchParams(
+      Object.fromEntries(headers),
+      process.env.SECRET_KEY ?? "defaultkey"
+    );
+    if (!verified) {
+      throw "Not verified";
+    }
+    return Response.json({
+      status: "success",
+      user: await prisma.user.findFirst({
+        where: { id: Number(headers.get("vk_user_id")) },
+      }),
+    });
+  } catch (e) {
+    console.error(e);
+    return Response.json({
+      status: "error",
+    });
+  }
+}
